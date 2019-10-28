3664b1e1431b2ea44a6f1bcad009f70c9cd08bb6
ashidasatoshi <dasatoshi2017@yahoo.co.jp>
2019-10-28 12:26:50 +0900

    controller作成,index,show,new,edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'properties/index'
+
+  get 'properties/show'
+
+  get 'properties/new'
+
+  get 'properties/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
